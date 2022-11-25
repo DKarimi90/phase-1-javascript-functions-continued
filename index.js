f709bed037a2fcf89a7add6234cb570de06a94af
david <davmwa90@gmail.com>
2022-11-25 16:05:07 +0300

    Defining different functions

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// code your solution here
+// defining a function  
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`; 
+}
+saturdayFun(); 
+
+// defining function as an expression 
+const mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`; 
+}
+mondayWork(); 
+
+// defining a function that returns another function 
+let wrapAdjective = function(flair = "*") {
+    return function(adjective){
+        return `You are ${flair}${adjective}${flair}!`; 
+    }
+}
